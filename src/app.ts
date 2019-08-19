73e245244e004bd3a27f91685ad8cc46b6976276
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-08-19 18:37:49 -0300

    Criação do servidor e query graphql

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("Olá")
+import * as express from 'express'
+import * as graphqlHTTP from 'express-graphql'
+import schema from './graphql/schema';
+
+class App {
+    public express: express.Application
+
+    constructor(){
+        this.express = express()
+        this.middleware()
+    }
+
+    private middleware():void {
+        this.express.use('/graphql', graphqlHTTP({
+            schema: schema,
+            graphiql: 1==1
+        }))
+    }
+}
+
+export default new App().express
